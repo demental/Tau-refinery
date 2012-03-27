f2b9213001e379565c931b8e12ad31a328276809
arnaud sellenet <arnodmental@gmail.com>
2012-03-27 11:49:46 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TauRefinery::Application.config.secret_token = 'fa58fcfbbe93fa234e5ea4f7e9deff31698c3422521c36d9cfce725912be65047697fa45a08cf0876188c7390546b36fd02239e4a7256d5da1e23cc1093f22f3'
